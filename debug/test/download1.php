2fdac6fb89f057d2994f16290adcfce14e9543c6
Masashi Otsuka <masashi1029xxx@gmail.com>
2018-12-12 22:04:44 +0900

    modify something

--- debug/test/download1.php
+++ debug/test/download1.php
@@ -1,10 +1,12 @@
 <?php
-$file_path = '../dlfile/;'
-$file_name = 'test1.txt'
+$file_name = 'test1.txt';
+$file_path = dirname(__FILE__).'/'.$file_name;
 // ダウンロードさせるファイル名
-header("Content-Type: application/octet-stream");
-header("Content-Disposition: attachment; filename=$file_name");
-readfile($file_path + $file_name;
+header('Content-Type: application/force-download;');
+//header("Content-Type: application/octet-stream");
+header('Content-Length: '.filesize($file_path));
+header('Content-Disposition: attachment; filename="'.$file_name.'"');
+readfile($file_name);
 //ファイル削除
 exit;
 ?>
