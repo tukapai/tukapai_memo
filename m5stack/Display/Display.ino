b5067f1fab3c68503ef068848c805a057e930bdb
tukapai <masashi1029xxx@gmail.com>
2019-01-17 23:19:11 +0900

    change Display.ino

--- m5stack/Display/Display.ino
+++ m5stack/Display/Display.ino
@@ -38,7 +38,7 @@ void setup() {
   M5.Lcd.drawTriangle(30, 30, 180, 100, 80, 150, YELLOW);
   delay(1000);
   M5.Lcd.fillTriangle(30, 30, 180, 100, 80, 150, YELLOW);  
-  M5.Lcd.fillTriangle(30, 30, 180, 100, 80, 150, BLUE);
+
 
 }
 
