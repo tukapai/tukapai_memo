19fe0e6c4dacf1679632d7a704e04392f94121a4
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-02-14 23:01:28 +0900

    Create IEsample.java

---
+++ Automation_test/IEsample.java
@@ -0,0 +1,47 @@
+package ~;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class IEtest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        System.setProperty("webdriver.ie.driver", "IEDriverSever.exeのパス");
+        driver = new InternetExplorerDriver();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+
+        try {
+
+            driver.get("URL");
+　　　　　　　/*このあとに何らかの処理～*/
+
+        }catch(IOException e){
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
