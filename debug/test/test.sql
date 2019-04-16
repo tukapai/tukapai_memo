7880f91349eeb66ef23a80ccb3b0e55122e3d4c2
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-04-16 23:22:59 +0900

    add test.sql

---
+++ debug/test/test.sql
@@ -0,0 +1,13 @@
+CREATE TABLE test
+(
+  id INT NOT NULL,
+  name VARCHAR NOT NULL,
+  publicdate DATE NOT NULL,
+  coverurl VARCHAR NOT NULL,
+  recommendpoint FLOAT NOT NULL,
+  summary VARCHAR NOT NULL,
+  impression INT NOT NULL,
+  readerlevel VARCHAR NOT NULL,
+  keyword VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
