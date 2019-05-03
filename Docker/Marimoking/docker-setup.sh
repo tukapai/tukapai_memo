d019dbd9e279f3ab265802314c49ebff3d61b92f
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-05-03 19:23:35 +0900

    Update docker-setup.sh

--- Docker/Marimoking/docker-setup.sh
+++ Docker/Marimoking/docker-setup.sh
@@ -3,6 +3,6 @@
 # check Docker version
 is_docker=`docker --version | awk '{ print $3 }' | sed '$s/.$//'`
 if [[ is_docker = ]]; then
- #statements
+ #コマンドがない場合はインストールするかとか考える
 fi
 docker-compose up
