eeabd13e2426cba4058f70f49c7e1bffe825202a
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-05-08 22:44:17 +0900

    Update docker-setup.sh

--- Docker/Marimoking/docker-setup.sh
+++ Docker/Marimoking/docker-setup.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 d_version='18.06.1-ce'
 # check Docker version
 v_docker=`docker --version | awk '{ print $3 }' | sed '$s/.$//'`
-if [[ v_docker != '18.06.1-ce' ]]; then
+if [[ v_docker != $d_version ]]; then
  echo "Your Docker version is not supported, please update"
 fi
 docker-compose up
