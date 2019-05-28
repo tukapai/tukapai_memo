d980f85b6487ba0fb0b2dfc5139fe90233d1fcc2
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-05-28 20:39:34 +0900

    modify docker-setup.sh

--- Docker/Marimoking/docker-setup.sh
+++ Docker/Marimoking/docker-setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-d_version='18.06.1-ce'
-# check Docker version
-v_docker=`docker --version | awk '{ print $3 }' | sed '$s/.$//'`
-if [[ v_docker != $d_version ]]; then
- echo "Your Docker version is not supported, please update"
-fi
+#TODO: バージョンチェック機能を入れる
+# d_version='18.06.1-ce'
+# # check Docker version
+# v_docker=`docker --version | awk '{ print $3 }' | sed '$s/.$//'`
+# if [[ v_docker != $d_version ]]; then
+#  echo "Your Docker version is not supported, please update"
+# fi
+mkdir MarimoKing
+cd MarimoKing
+echo "SetUp!!MarimoKing!!"
+curl -O https://raw.githubusercontent.com/tukapai/tukapai_memo/master/Docker/Marimoking/docker-compose.yml
 docker-compose up
