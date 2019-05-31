d65dc47779c578e365046daef027649f3562072f
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-05-31 22:45:46 +0900

    Update vbox_snap.rb

--- debug/Ruby/vbox_snap.rb
+++ debug/Ruby/vbox_snap.rb
@@ -4,9 +4,8 @@ require 'date'
 #
 # @param
 # v_snap: スナップショットのリスト
-# t_vm: 対象とするVM名
+# 
+
 def snap_add(vm_name)
- `VBoxManage snapshot "$vm_name" take  --live`
+ `VBoxManage snapshot "#{vm_name}" take  --live`
 end
-snapshot_list =　`VBoxManage snapshot  "CentOS" list`
-snapshot
