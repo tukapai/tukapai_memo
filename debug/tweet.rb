bb515750c9c56720444b5dcdf9f6fac647f7beb4
tukapai <masashi1029xxx@gmail.com>
2019-07-18 23:47:31 +0900

    add tweet.rb

---
+++ debug/tweet.rb
@@ -0,0 +1,12 @@
+require 'twitter'
+
+@client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
+end
+
+p @client.user.name
+p @client.user.description
+p @client.user.tweets_count
