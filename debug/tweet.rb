0a7114d2a0ee26c5329d74e858ca1921bf7299c7
Masashi Otsuka <masashi1029xxx@gmail.com>
2019-09-16 15:11:43 +0900

    🚧 Update twitter.rb

--- debug/tweet.rb
+++ debug/tweet.rb
@@ -11,7 +11,16 @@ p @client.user.name
 p @client.user.description
 p @client.user.tweets_count
 
+# put tweet
 def tweet
   @client.update(ARGV[0])
   puts "Tweetしたぞい"
+end
+
+# display timeline
+def homeTimeline
+  @client.home_timeline.each do |tweet|
+    puts "\e[33m" + tweet.user.name + "\e[32m" + "[ID:" + tweet.user.screen_name + "]"
+    puts "\e[0m" + tweet.text
+  end
 end
